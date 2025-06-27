662248b49331584af4c02af697d387923d7c814d
nehatech <menehalalwani@gmail.com>
2025-06-27 17:13:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import {useState} from 'react';
+import './App.css';
+
+function App() {
+  const [todoList,setTodoList] = useState([]);
+  const [newTask, setNewTask ] = useState("");
+  const handleChanges = (event) =>{
+    setNewTask(event.target.value);
+  };
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+    }
+    setTodoList([...todoList, task]);
+  };
+  const deleteTask = (id) =>{
+
+      setTodoList( todoList.filter((task) => task.id !== id));
+  };
+  return(
+    <div className='App'>
+    <div className='addTask'>
+    <input onChange={handleChanges}/>
+    
+    <button onClick={addTask}>Add Task</button>
+    </div>
+    <div className='list'>
+      {todoList.map((task) =>
+      {
+        return( 
+        <div>
+          <h2>{task.taskName}</h2>
+          <button onClick={() => deleteTask(task.id)}> X </button> 
+        </div>);
+      })}
+    </div>
+   
+    </div>
+  )
+    
+}
+
+export default App;
